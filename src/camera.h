0fda07572d3f23de414a8d72eaea0a374c1d880d
mmaster <myles1551@gmail.com>
2024-12-29 15:02:31 -0600

    I cant take it anymore

--- src/camera.h
+++ src/camera.h
@@ -1,13 +1,23 @@
 #pragma once
 
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 class Camera {
 public:
   Camera();
   ~Camera();
 
-private:
-  float FOV_;
-  // float znear, zfar;
+  const glm::mat4& getProjection() const { return projectionMatrix; }
 
-  void updateViewMatrix();
+  void setOrthographicProjection(float left, float right, float top, float bottom, float near, float far);
+  void setPerspectiveProjection(float fovY, float aspect, float near, float far);
+
+
+private:
+  glm::mat4 projectionMatrix{1.0f};
+
+
+  //void updateViewMatrix();
 };
